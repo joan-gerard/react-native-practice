261802dde2dcda7eed15bf21eb4cc2b143390f06
Joan Gerard <joan.gerard@outlook.com>
2024-11-30 16:34:39 +0100

    Setup

---
+++ app/index.tsx
@@ -0,0 +1,25 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { Link } from "expo-router";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>index!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{ color: "blue" }}>
+        Go to profile
+      </Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#ffff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
